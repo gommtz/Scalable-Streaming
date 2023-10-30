2f3495cd9de1ead5e798f90ca785bd7e981e9328
victor Gomez <victor.gommtz@gmail.com>
2023-10-30 12:45:43 -0600

    Convert OpenAI Raw Stream into a Python Generator Function

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -8,12 +8,9 @@ GENERATION_TIMEOUT_SEC = 60
 async def stream_generator(subscription):
     async with async_timeout.timeout(GENERATION_TIMEOUT_SEC):
         try:
-            async for chunk in subscription:
-                yield post_processing(chunk)
+            async for event in subscription:
+                if "content" in event["choices"][0].delta:
+                    current_response = event["choices"][0].delta.content
+                    yield current_response
         except asyncio.TimeoutError:
             raise HTTPException(status_code=504, detail="Stream timed out")
-
-
-async def post_processing(chunk):
-    print(chunk)
-    return chunk
