9066954624471dd810713d81a58b62e400630145
victor Gomez <victor.gommtz@gmail.com>
2023-10-30 11:48:33 -0600

    delete request properties not used and set gpt-4 as default model

--- src/models/inference_request.py
+++ src/models/inference_request.py
@@ -3,8 +3,6 @@ from typing import Optional
 
 
 class InferenceRequest(BaseModel):
-    model_name: str
+    model_name: Optional[str] = "gpt-4"
     input_text: str
-    api_key: Optional[str]
-    org_id: Optional[str] = None
     generation_cfg: Optional[dict] = None
